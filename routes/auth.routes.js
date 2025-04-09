22868a242464fdcad4b430e9e1b0d77a62bb2418
Thanos0908 <athanasiosnouskalis@gmail.com>
2025-04-09 21:28:32 +0300

    Google Login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
+router.get('/google/callback', authController.googleLogin);
 
 module.exports = router
+
+// https://accounts.google.com/o/oauth2/auth?client_id=366353755967-joih7lupv86tge9th4iagse1a3klh61d.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
